3e1ef4b34e7b6cf3acf3a38f5a1a41b01ea884f6
ipaulhamus <143576350+ipaulhamus@users.noreply.github.com>
2024-12-05 15:09:08 -0500

    Addition function.

--- YearPercentage/YearPercentage/Program.cs
+++ YearPercentage/YearPercentage/Program.cs
@@ -41,5 +41,12 @@
                 num2 = 0;
             }
         }
+
+        public void DoMath(int num1, int num2)
+        {
+            int result = num1 + num2;
+
+            Console.WriteLine($"\n----\nThe result of Adding the numbers is '{result}'----\n");
+        }
     }
 }
